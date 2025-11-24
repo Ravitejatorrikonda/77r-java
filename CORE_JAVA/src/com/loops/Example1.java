9b773461bd2f9d4760542de5cc5d89e414a07bbe
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2025-11-24 08:29:51 +0530

    Loops

---
+++ CORE_JAVA/src/com/loops/Example1.java
@@ -0,0 +1,62 @@
+package com.loops;
+
+public class Example1 {
+//	public void printTable(int num, int value) {
+//		System.out.println(num+"x"+value+"="+num*value);
+//		
+//	}
+	
+//	public void loopMethods() {
+//		for(int step=1; step<=5; step++) {
+//			System.out.println("Count Steps: "+step);
+//		}
+//	}
+	
+	public void  trickyLoops() {
+//		int i=1;
+//		for(;i<=10;i++) {
+//			System.out.println(i);
+//		}
+		
+		
+//		for(int a=1;a<=5;) {
+//			System.out.println(a);
+//		}
+		
+		
+//		for(int a=5;;a*=2) {
+//			System.out.println(a);
+//		}
+		
+		
+		
+//		for(int a=5;true;a++) {
+//			System.out.println("Hello There");
+//		}
+//		System.out.println("Hello there");
+		
+//		for(int i=5; i>=1; i--) {
+//			System.out.println(i);
+//		}
+		for (int i=4; i<=16; i++) {
+			System.out.println(i*2);
+		}
+		
+	}
+	/**
+	 * write a java program print the number 1 to 5 in a reverse order;
+	 * 
+	 * write a java program print numbers from 4 to 16 and multiplication 2 in each number
+	 * 
+	 * 
+	 */
+
+	public static void main(String[] args) {
+		Example1 obj=new Example1();
+//		obj.loopMethods();
+		obj.trickyLoops();
+		
+
+	}
+
+}

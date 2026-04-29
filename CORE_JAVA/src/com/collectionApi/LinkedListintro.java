410c8ad18f406e64555eaef5b12638a01d85057f
Ravitejatorrikionda <ravitejatorrikonda@gmail.com>
2026-04-29 15:08:13 +0530

    LinkedList,vector

---
+++ CORE_JAVA/src/com/collectionApi/LinkedListintro.java
@@ -0,0 +1,65 @@
+package com.collectionApi;
+
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Vector;
+
+public class LinkedListintro {
+
+	public static void main(String[] args) {
+		
+		
+		
+		Vector<Integer> v=new Vector<Integer>();
+		
+		v.add(34);
+		v.add(43);
+		v.add(43);
+		
+		System.out.println(v);
+		
+		Enumeration<Integer> en=v.elements();
+		while (en.hasMoreElements()) {
+			Integer integer =  en.nextElement();
+			System.out.println(integer);
+			
+			
+		}
+		System.out.println(v);
+		
+		
+		
+//	    List l=new LinkedList();
+//	        l.add(34);
+//	        l.add(43);
+//	        l.add(43);
+//	        l.add("hello");
+//
+//	        System.out.println(l);
+//	        
+//	        ListIterator<Integer> li=l.listIterator(l.size());
+//	        while(li.hasPrevious()) {
+//	        	System.out.println(li.previous());
+//	        }
+	        
+	        
+	        
+	        
+	        
+	        
+	        
+//	        while (li.hasNext()) {
+//				  System.out.println(li.next());
+//
+//			}
+//	        l.remove(0);
+//	        System.out.println(l);
+	    
+
+	}
+
+}
